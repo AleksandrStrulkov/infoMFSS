cfae08a2d72ed0b271a2e0bcb1329e5f5ac0decd
Aleksandr Strulkov <astrulkov@yandex.ru>
2024-12-11 18:21:14 +0300

    change urls.py

--- infoMFSS/urls.py
+++ infoMFSS/urls.py
@@ -1,6 +1,6 @@
 from infoMFSS.apps import InfomfssConfig
 from django.urls import path
-from infoMFSS.views import MFSSTemplateView, percent_view, MyListView
+from infoMFSS.views import MFSSTemplateView, percent_view, EquipmentListView, CableListView
 
 # from mfss.views import SubsystemTemplateView
 # from infoMFSS.views import index
@@ -10,5 +10,6 @@ app_name = InfomfssConfig.name
 urlpatterns = [
     path('', MFSSTemplateView.as_view(), name='home'),
     path('percents/', percent_view, name='percents'),
-    path('equipment_list/', MyListView.as_view(), name='equipment')
+    path('equipment_list/', EquipmentListView.as_view(), name='equipment'),
+    path('cable_list/', CableListView.as_view(), name='cable')
 ]
