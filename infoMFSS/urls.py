ff2bff90297b796790cbcf1a0f4f0cfc1f807f83
Aleksandr Strulkov <astrulkov@yandex.ru>
2025-02-06 19:06:46 +0300

    add captcha

--- infoMFSS/urls.py
+++ infoMFSS/urls.py
@@ -2,9 +2,9 @@ from django.views.generic import TemplateView
 
 from infoMFSS.apps import InfomfssConfig
 from django.urls import path
-from infoMFSS.views import percent_view, EquipmentListView, CableListView, BoxListView, \
+from infoMFSS.views import EquipmentListView, CableListView, BoxListView, \
     EquipmentFileListView, CableFileListView, ViolationsListView, ProjectEquipmentListView, \
-    ProjectCableListView, MFSSPercentTemplateView, ContactFormView, VisualView
+    ProjectCableListView, MFSSPercentTemplateView, ContactFormView, VisualView, PercentView, QuantityEquipmentCableView
 
 # from mfss.views import SubsystemTemplateView
 # from infoMFSS.views import index
@@ -13,7 +13,7 @@ app_name = InfomfssConfig.name
 
 urlpatterns = [
         path('', MFSSPercentTemplateView.as_view(), name='home'),
-        path('percents/', percent_view, name='percents'),
+        path('percents/', PercentView.as_view(), name='percents'),
         path('equipment_list/', EquipmentListView.as_view(), name='equipment'),
         # path('equipment_list/', EquipmentView.as_view(), name='equipment'),
         path('cable_list/', CableListView.as_view(), name='cable'),
@@ -25,5 +25,11 @@ urlpatterns = [
         path('project_equipment/', ProjectEquipmentListView.as_view(), name='project_equipment'),
         path('project_cable/', ProjectCableListView.as_view(), name='project_cable'),
         path('contact/', ContactFormView.as_view(), name='contact'),
-        path('contact/success/', TemplateView.as_view(template_name='mfss/contact_success.html'), name='contact_success'),
+        path(
+            'contact/success/', TemplateView.as_view(template_name='mfss/contact_success.html'), name='contact_success'
+            ),
+        path(
+            'quantity/', QuantityEquipmentCableView.as_view(template_name='mfss/quantity_equipment_cable.html'),
+            name='quantity_equipment_cable'
+            ),
 ]
