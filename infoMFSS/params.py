69b6b4533deaac5d2db4714737bd63e78f08ccd1
Aleksandr Strulkov <astrulkov@yandex.ru>
2025-02-06 19:02:53 +0300

    add params.py

--- infoMFSS/params.py
+++ infoMFSS/params.py
@@ -1,29 +1,3 @@
-# class FilterParams:
-#     def __init__(self, request):
-#         # Значения по умолчанию - "Все..."
-#         self.mine = request.GET.get("mine")
-#         self.subsystem = request.GET.get("subsystem")
-#         self.incl_blocks = request.GET.get("incl_blocks")
-#         self.equipment = request.GET.get("equipment")
-#
-#         # Проверяем, были ли переданы параметры формы
-#         self.is_form_submitted = any(
-#                 [
-#                         self.mine is not None,
-#                         self.subsystem is not None,
-#                         self.incl_blocks is not None,
-#                         self.equipment is not None
-#                 ]
-#         )
-#
-#     def to_dict(self):
-#         return {
-#                 "mine": self.mine,
-#                 "subsystem": self.subsystem,
-#                 "incl_blocks": self.incl_blocks,
-#                 "equipment": self.equipment,
-#         }
-
 class FilterParams:
     def __init__(self, request):
         self.request = request
