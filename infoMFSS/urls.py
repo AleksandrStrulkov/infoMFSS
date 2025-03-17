77ab4a89beb537e4eaf72395ec49eac78355d48b
Aleksandr Strulkov <astrulkov@yandex.ru>
2025-03-17 21:54:27 +0300

    Change urls.py

--- infoMFSS/urls.py
+++ infoMFSS/urls.py
@@ -7,7 +7,8 @@ from infoMFSS.views import EquipmentListView, CableListView, BoxListView, \
     ProjectCableListView, MFSSPercentTemplateView, ContactFormView, PercentView, QuantityEquipmentCableView, \
     CreateEquipmentView, CreateCableView, CreatePointPhoneView, CreateBranchesBoxView, CreateCableMagazineView, \
     CreateViolationsView, VisualView, CreateVisualView, CreateEquipmentInstallationView, CreateExecutionView, \
-    EquipmentView, UpdateEquipmentView, DeleteEquipmentView
+    EquipmentView, UpdateEquipmentView, DeleteEquipmentView, CableView, UpdateCableView, DeleteCableView, \
+    PointPhoneView, UpdatePointPhoneView, DeletePointPhoneView
 
 # from mfss.views import SubsystemTemplateView
 # from infoMFSS.views import index
@@ -34,6 +35,7 @@ urlpatterns = [
             'quantity/', QuantityEquipmentCableView.as_view(template_name='infoMFSS/quantity_equipment_cable.html'),
             name='quantity_equipment_cable'
             ),
+        # Создание (только для модератора)
         path('moderator/create/equipment/', CreateEquipmentView.as_view(), name='create_equipment'),
         path('moderator/create/cable/', CreateCableView.as_view(), name='create_cable'),
         path('moderator/create/pointphone/', CreatePointPhoneView.as_view(), name='create_pointphone'),
@@ -44,7 +46,16 @@ urlpatterns = [
         path('moderator/create/equipment-installation/', CreateEquipmentInstallationView.as_view(),
              name='create_equipment_installation'),
         path('moderator/create/execution/', CreateExecutionView.as_view(), name='create_execution'),
+        # Просмотр и выбор для редактирования и удаления (только для модератора)
         path('moderator/list/equipment/', EquipmentView.as_view(), name='equipment_list'),
+        path('moderator/list/cable/', CableView.as_view(), name='cable_list'),
+        path('moderator/list/point_phone/', PointPhoneView.as_view(), name='point_phone_list'),
+        # Редактирование (только для модератора)
         path('moderator/update/equipment/<int:pk>', UpdateEquipmentView.as_view(), name='equipment_update'),
+        path('moderator/update/cable/<int:pk>', UpdateCableView.as_view(), name='cable_update'),
+        path('moderator/update/point_phone/<int:pk>', UpdatePointPhoneView.as_view(), name='point_phone_update'),
+        # Удаление (только для модератора)
         path('moderator/delete/equipment/<int:pk>', DeleteEquipmentView.as_view(), name='equipment_delete'),
+        path('moderator/delete/cable/<int:pk>', DeleteCableView.as_view(), name='cable_delete'),
+        path('moderator/delete/point_phone/<int:pk>', DeletePointPhoneView.as_view(), name='point_phone_delete'),
 ]
