b5192ac0089c3eee9e9795bc8b713ff8ee2c407f
Aleksandr Strulkov <astrulkov@yandex.ru>
2025-07-09 20:48:12 +0300

    update create_users.py

--- users/management/commands/create_users.py
+++ users/management/commands/create_users.py
@@ -11,32 +11,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 
 
-# class Command(BaseCommand):
-#
-#     def handle(self, *args, **options):
-#         user = User.objects.create(
-#                 # email='admin@infomfss.ru',
-#                 email=os.getenv('ADMIN_EMAIL'),
-#                 # first_name='Иван',
-#                 first_name=os.getenv('ADMIN_FIRST_NAME'),
-#                 # middle_name='Иванович',
-#                 middle_name=os.getenv('ADMIN_MIDDLE_NAME'),
-#                 # last_name='Иванов',
-#                 last_name=os.getenv('ADMIN_LAST_NAME'),
-#                 is_staff=True,
-#                 is_superuser=True,
-#                 is_active=True
-#         )
-#         user.set_password(os.getenv('ADMIN_PASSWORD'))
-#         allowed_person = AllowedPerson.objects.create(
-#                 first_name=os.getenv('ADMIN_FIRST_NAME'),
-#                 middle_name=os.getenv('ADMIN_MIDDLE_NAME'),
-#                 last_name=os.getenv('ADMIN_LAST_NAME'),
-#         )
-#         user.save()
-#         allowed_person.save()
-
-
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if User.objects.filter(email=os.getenv("ADMIN_EMAIL")).exists():
